4ec95b590cf5a9ee217fb0702fee130afb5edaff
handuy <handuy1992@gmail.com>
2018-02-03 11:32:13 +0700

    Exercise 2

---
+++ Codecamp-3/Exercise_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() {};
+            List<int> squares = new List<int>() {};
+            var result = TestForSquares(numbers, squares);
+            Console.WriteLine(result);
+        }
+
+        public static bool TestForSquares(IEnumerable<int> numbers, IEnumerable<int> squares)
+        {
+            var squareResult = numbers.Select(num => num*num).ToList();
+            var squareList = squares.ToList();
+            bool checkResult = squareResult.SequenceEqual(squareList);
+            return checkResult;
+        }
+    }
+}
---
+++ Codecamp-3/Exercise_2/ReadMe.md
@@ -0,0 +1,9 @@
+﻿Viết 1 phương thức kiểm tra 2 tập hợp dữ liệu: sử dụng LINQ không dùng vòng lặp
+// Phương thức dưới đây trả về True khi tất cả các phần tử trong tập hợp squares
+// bằng với bình phương của các phần tử trong tập hợp numbers
+/* Ví dụ Test cases:
+TestForSquares({1, 2, 3, 4, 5}, {1, 4, 9, 16, 25}) -> True
+TestForSquares({1, 2, 5}, {1, 4, 9}) -> False
+TestForSquares({}, {}) -> True
+TestForSquares({1, 5, 3}, {1, 25, 9, 9}) -> False
+*/

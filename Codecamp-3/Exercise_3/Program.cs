f2af61ff71b857a86fdf523231c58ef5ef8df7b5
handuy <handuy1992@gmail.com>
2018-02-03 12:24:49 +0700

    Exercise 3

---
+++ Codecamp-3/Exercise_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listWord = new List<string>(){"he","she","it","we","you","they"};
+            var result = GetWord(listWord);
+            if (result.Count == 0) {
+                Console.WriteLine("null");
+            } else {
+                var lastWord = result[result.Count - 1];
+                Console.WriteLine($"The last word is {lastWord}");
+            }
+        }
+
+        public static List<string> GetWord(IEnumerable<string> words)
+        {
+            var result = words.Where(item => item.IndexOf('e') != -1).OrderBy(item => item).ToList();
+            return result;
+        }
+    }
+}
